ccada02ccbe14c54aaaeb018fdd57cec558ea5ae
adityamo <rfadityahalim18@gmail.com>
2022-10-06 17:02:40 +0700

    add 404 Page

---
+++ pages/404.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Custom404() {
+  return (
+    <div>
+        <h1 className='title-not-found'>Opsss..</h1>
+        <h3 className='title-not-found'>Halaman yang anda cari tidak ada</h3>
+    </div>
+  )
+}
