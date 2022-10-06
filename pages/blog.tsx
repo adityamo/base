2e6ea8a06fdea3d03974f9f3144c3d31209fd092
adityamo <rfadityahalim18@gmail.com>
2022-10-06 21:59:56 +0700

    membuat meta data dirumah

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -3,7 +3,7 @@ import Layout from "../components/layout";
 
 export default function Blog(){
    return (
-    <Layout>
+    <Layout pageTitle="Blog">
         <p className="title">Blog Page</p>
     </Layout>
         
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,16 @@
 
 import styles from '../styles/Home.module.css'
 import Layout from '../components/layout'
+import Head from 'next/head';
 
 export default function Home() {
   return(
-    <Layout>
+   
+      <Layout pageTitle = "HomePage">
         <h1 className={styles['title-homepage']}>Welcome</h1>
-    </Layout>
+      </Layout>
 
+  
+   
   );
 }
